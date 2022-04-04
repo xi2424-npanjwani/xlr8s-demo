f5dca92e4be4c0240a9f2817fa1d0054394c7542
xlr8s <xlr8s@xebia.com>
2022-04-04 08:38:28 +0000

    added backend.tf

--- envs/dev/01-infra/my-vpc/backend.tf
+++ envs/dev/01-infra/my-vpc/backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {	
     bucket = "xlr8s-artifacts"
     encrypt = "false"
-    key = "envs/dev/01-infra/my-vpc/terraform.tfstate"
+    key = "xlr8s-demo-9723/envs/dev/01-infra/my-vpc/terraform.tfstate"
     region = "ap-south-1"
     role_arn = ""
   }
